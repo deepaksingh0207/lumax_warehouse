47e352835a37fa305c3e5a5c65394c8d76262dd1
HerambGite-FT <herambg@fts-pl.com>
2025-09-20 18:37:35 +0530

    started with import users via csv file

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.app')
 @section('content')
-<h1 class="app-page-title">Users</h1>
+<div class="row g-3 mb-4 align-items-center justify-content-between">
+    <div class="col-auto">
+        <h1 class="app-page-title mb-0">Users</h1>
+    </div>
+    <div class="col-auto">
+        <div class="page-utilities">
+            <div class="row g-2 justify-content-start justify-content-md-end align-items-center">
+                <div class="col-auto">
+                    <input type="file" class="d-none" name="" id="import_file">
+                    <a class="btn app-btn-primary" href="#">
+                        <i class='fas fa-arrow-up'></i>
+                        Import Users
+                    </a>
+                </div>
+            </div><!--//row-->
+        </div><!--//table-utilities-->
+    </div><!--//col-auto-->
+</div>
 <div class="row">
     <div class="app-card app-card-orders-table shadow-sm mb-5">
         <div class="app-card-body">
@@ -8,6 +25,7 @@
                 <table class="table app-table-hover mb-0 text-left">
                     <thead>
                         <tr>
+                            <th>Group</th>
                             <th class="cell">SAP Code</th>
                             <th class="cell">Name</th>
                             <th class="cell">Email</th>
@@ -15,20 +33,32 @@
                         </tr>
                     </thead>
                     <tbody>
+                        @forelse ($users as $user)
                         <tr>
-                            <td class="cell">000015346</td>
-                            <td class="cell"><span class="truncate">Admin User</span></td>
-                            <td class="cell">admin@lumaxworld.in</td>
+                            <td class="cell">{{ $user->group->name }}</td>
+                            <td class="cell">{{ $user->sap_code }}</td>
+                            <td class="cell"><span class="truncate">{{ $user->name }}</span></td>
+                            <td class="cell">{{ $user->email }}</td>
                             <td class="cell">
                                 <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" role="switch" id="mySwitch" checked>
-                                <label class="form-check-label" for="mySwitch" id = "switch_label_">Active</label>
+                                    <input class="form-check-input" type="checkbox" role="switch" id="switch_checkbox_{{ $user->id }}" checked>
+                                    <label class="form-check-label" for="mySwitch" id="switch_label_{{ $user->id }}">Active</label>
                                 </div>
                             </td>
                         </tr>
+                         @empty
+                        <tr>
+                            <td colspan="4" class="text-center">No Data found</td>
+                        </tr>
+                        @endforelse
 
                     </tbody>
                 </table>
+                <hr>
+                <!-- Pagination Links -->
+                <div>
+                    {{ $users->withQueryString()->links() }}
+                </div>
             </div><!--//table-responsive-->
 
         </div><!--//app-card-body-->
